2a57c35b4e31baa34d17052f3f34e057620ffe4e
Igor Matos <igormspc@gmail.com>
2019-05-26 13:42:57 -0300

    handle keyboard return click on login

--- agora/LoginViewController.swift
+++ agora/LoginViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
-class LoginViewController: UIViewController {
-    @IBOutlet var handle: UITextField!
-    @IBOutlet var password: UITextField!
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
     
     @IBAction func createAccount(_ sender: Any) {
         performSegue(withIdentifier: "signupsegue", sender: self)
@@ -10,15 +10,44 @@ class LoginViewController: UIViewController {
     
     @IBAction func login(_ sender: Any) {
         // [FINAL] go RED and display message if login is incorrect
-        guard let handle = handle.text else {return}
-        guard let userInDatabase = users[handle] else {return}
-        guard userInDatabase.password == password.text else {return}
+        doLogin()
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction.init(title: "Cancelar", style: .cancel, handler: { _ in
+            //
+        }))
+        self.present(alertController, animated: false)
+    }
+    
+    func doLogin() {
+        guard let username = usernameField.text, let password = passwordField.text else {
+            showAlert(title: "Preencha todos campos", message: "Verifique se todos campos foram escritos")
+            return
+        }
         
-        activeUser = handle
+        guard let userInDatabase = users[username], userInDatabase.password == password else {
+            showAlert(title: "Dados incorretos", message: "Verifique as informações")
+            return
+        }
+        
+        activeUser = username
         performSegue(withIdentifier: "loggedinsegue", sender: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameField.delegate = self
+        passwordField.delegate = self
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() // Dismiss the keyboard
+        
+        doLogin()
+        return true
+    }
+    
 }
