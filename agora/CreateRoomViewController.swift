2a5f05e8dc20bc3632b15da85e72e60cd9127937
Igor Matos <igormspc@gmail.com>
2019-05-26 16:42:10 -0300

    init CreateRoomViewController

---
+++ agora/CreateRoomViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateRoomViewController.swift
+//  agora
+//
+//  Created by Igor Matos  on 5/26/19.
+//  Copyright © 2019 agora. All rights reserved.
+//
+
+import UIKit
+
+class CreateRoomViewController: AgoraViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backClick(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
