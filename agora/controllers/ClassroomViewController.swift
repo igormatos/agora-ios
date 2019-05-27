fb953fda02b55674bb093c61bf16dde054b596dc
Igor Matos <igormspc@gmail.com>
2019-05-27 08:44:45 -0300

    get classroom from firebase

--- agora/controllers/ClassroomViewController.swift
+++ agora/controllers/ClassroomViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 
 class ClassroomViewController: AgoraViewController {
     
+    @IBOutlet weak var clasroomName: UILabel!
     @IBOutlet weak var peopleCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let classroomId = AppSingleton.shared().loggedRoom?.code
+        if let classroomId = classroomId {
+            FirebaseHelper.shared().getClassroom(id: classroomId, onError: { error in
+                self.showAlert(title: "Erro ao carregar sala", message: error)
+            }) { classroom in
+                self.clasroomName.text = classroom.name
+                
+                self.peopleCountLabel.text = "\(classroom.users.count) PESSOAS NA SALA"
+            }
+        }
+        
     }
 
 }
