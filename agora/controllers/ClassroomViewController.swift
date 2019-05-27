eb9220ff3ea0d27c989242aa5abeeff10e112fdf
Igor Matos <igormspc@gmail.com>
2019-05-27 00:12:06 -0300

    FirebaseHelper

---
+++ agora/controllers/ClassroomViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ClassroomViewController.swift
+//  agora
+//
+//  Created by Igor Matos  on 5/26/19.
+//  Copyright © 2019 agora. All rights reserved.
+//
+
+import UIKit
+
+
+class ClassroomViewController: AgoraViewController {
+    
+    @IBOutlet weak var peopleCountLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
