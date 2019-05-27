9f9ab4bde6b3e2486fd46e1f05f6dd6a40471caf
Igor Matos <igormspc@gmail.com>
2019-05-26 21:30:36 -0300

    show code after classroom has been created

---
+++ agora/controllers/GetCodeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GetCodeViewController.swift
+//  agora
+//
+//  Created by Igor Matos  on 5/26/19.
+//  Copyright © 2019 agora. All rights reserved.
+//
+
+import UIKit
+
+class GetCodeViewController: AgoraViewController {
+
+    @IBOutlet weak var roomCodeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let roomCode = AppSingleton.shared().loggedRoom?.code else {
+            return
+        }
+        
+        roomCodeLabel.text = roomCode
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
