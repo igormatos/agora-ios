a4b7da8cc7695d95ed0189220effaa54b9e7bb1d
Igor Matos <igormspc@gmail.com>
2019-05-28 14:37:43 -0300

    feedback to user that code has been copied

--- agora/controllers/GetCodeViewController.swift
+++ agora/controllers/GetCodeViewController.swift
@@ -24,17 +24,7 @@ class GetCodeViewController: AgoraViewController {
 
     @IBAction func copyCodeClick(_ sender: Any) {
         UIPasteboard.general.string = roomCodeLabel.text!
+        self.showAlert(title: "Código copiado!", message: "Envie para outras pessoas se inscreverem na sala")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
